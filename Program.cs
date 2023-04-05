a599055498c8aa8af2b7468ed67d1c639a6ca46f
michael.lewellen <michael.lewellen@snow.edu>
2023-04-05 14:20:27 -0600

    Working Asserts

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Debug.Assert(isPallendromic("mom") == true, "mom is Pallendromic");
+        Debug.Assert(isPallendromic("madam")== true, "madam is Pallendromic");
+        Debug.Assert(isPallendromic("bill")==false, "bill is Not Pallendromic");
+        Debug.Assert(isPallendromic("Mom")==true, "Mom is pallendromic");
+        Debug.Assert(isPallendromic("Madam I'm Adam")== true, "Madam I'm Adam is pallendromic");
+        Debug.Assert(isPallendromic("A man, a plan, a canal: Panama")==true);
+        static bool isPallendromic(string s)
+        {
+            s = s.ToLower();
+            string[] temp = s.Split(" ");
+            s = String.Join("",temp);               
+            string reverse="";
+            List<char> charArray= new List<char>();
+            for(int i = 0; i< s.Length; i++)
+            {
+                if(s[i] >= 97 && s[i] <= 122)
+                 {
+                    reverse = s[i] + reverse;
+                    charArray.Add(s[i]);
+                 }
+
+            }
+            s = new string(charArray.ToArray());
+            
+            Console.Write($"{s} {reverse}");
+            Console.WriteLine();
+            if (s == reverse)
+            {
+                //Console.WriteLine($"{s} is pallendromic");
+                return true;
+            }
+            else 
+            {
+                //Console.WriteLine($"{s} is not pallendromic");
+                return false;
+            }
+        }
+    }   
+}
